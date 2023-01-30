337c243c10597a8dd470db49371e7753496c86e8
Stefan Ivaylov Stoyanov <stefan05ivaylov@gmail.com>
2023-01-30 12:51:01 +0200

    Generated new tests

---
+++ Generated-tests/gen_test.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+char base[4] = {'A', 'C', 'G', 'T'};
+string int_to_string(int num){
+    string s="";
+    if(num==0)
+        return "0";
+    while (num)
+    {
+        s=(char)(num%10+(int)'0')+s;
+        num/=10;
+    }
+    return s;
+    
+}
+string genstring(int n){
+    string s="";
+    int num;
+    for(int i=0;i<n;++i){
+        num=rand()%4;
+        s+=base[num];
+    }
+    return s;
+}
+void gentest(int test,int limit){
+    string testname="input";
+    testname+=int_to_string(test);
+    testname+=".txt";
+    cout<<"testname: "<<testname<<endl;
+    fstream fout;
+    fout.open(testname, ios_base::app);
+    //fout.open("input.txt");
+    int n=rand()%limit+30;
+    string ref=genstring(n);
+    fout<<ref<<"\n";
+    int testcase=15,len;
+    fout<<testcase<<"\n";
+    string query;
+    for(int i=0;i<testcase;++i){
+        query="";
+        len=rand()%(n-10);
+        if(len==0)len=1;
+        query=genstring(len);
+        fout<<query<<"\n";
+    }
+    fout.close();
+}
+int main(){
+    srand(time(0));
+    int numtests,limit;
+    //cin>>numtests>>limit;
+    cout<<"Enter numtests: ";
+    cin>>numtests;
+    cout<<"Enter limit: ";
+    cin>>limit;
+    for(int test=1;test<=numtests;++test)
+        gentest(test,limit);
+    cout<<"End of the program\n";
+    return 0;
+}
